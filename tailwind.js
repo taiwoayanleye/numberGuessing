d5d1b3241a8c4ffe1efa8a4a95e3e019bd9eed70
Taiwo Ayanleye <mrtaiwoayanleye@gmail.com>
2024-10-14 10:30:55 -0400

    Rearrange files.

---
+++ tailwind.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        black: "#1E1E1E",
+        gray: "#8D99AE",
+        white: "#EDF2F4",
+        green: "#23CE6B",
+        yellow: "#EAC435",
+        red: "#E40066",
+      },
+    },
+  },
+};
